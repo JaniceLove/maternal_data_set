cd0650eadb2d2ed84017eb3dcc709700f4d057a5
janicelove <jlove5@nd.edu>
2017-10-21 12:04:40 -0400

    to save your chrom data to file

---
+++ save_to_file.py
@@ -0,0 +1,91 @@
+out_file = open ("two", 'w')
+two.to_csv("two", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("three", 'w')
+three.to_csv("three", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("four", 'w')
+four.to_csv("four", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("five", 'w')
+five.to_csv("five", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("six", 'w')
+six.to_csv("six", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("seven", 'w')
+seven.to_csv("seven", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("eight", 'w')
+eight.to_csv("eight", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("nine", 'w')
+nine.to_csv("nine", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("ten", 'w')
+ten.to_csv("ten", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("eleven", 'w')
+eleven.to_csv("eleven", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("twelve", 'w')
+twelve.to_csv("twelve", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("thirteen", 'w')
+thirteen.to_csv("thirteen", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("fourteen", 'w')
+fourteen.to_csv("fourteen", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("fifteen", 'w')
+fifteen.to_csv("fifteen", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("sixteen", 'w')
+sixteen.to_csv("sixteen", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("seventeen", 'w')
+seventeen.to_csv("seventeen", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("eighteen", 'w')
+eighteen.to_csv("eighteen", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("nineteen", 'w')
+nineteen.to_csv("nineteen", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("twenty1", 'w')
+twenty1.to_csv("twenty1", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("twenty2", 'w')
+twenty2.to_csv("twenty2", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("twenty3", 'w')
+twenty3.to_csv("twenty3", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("twenty4", 'w')
+twenty4.to_csv("twenty4", sep = "\t", header = None, index = False)
+out_file.close() 
+
+out_file = open ("twenty5", 'w')
+twenty5.to_csv("twenty5", sep = "\t", header = None, index = False)
+out_file.close() 
