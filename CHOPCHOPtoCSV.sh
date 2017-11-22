4a989579f7e7d3bd552229254a856bf28f86fbe6
janicelove <jlove5@nd.edu>
2017-11-22 12:22:55 -0500

    added user instructions

--- CHOPCHOPtoCSV.sh
+++ CHOPCHOPtoCSV.sh
@@ -1,4 +1,7 @@
 #script to convert CHOP CHOP results table to proper csv functional file
+#Select "results table" from "download results" drop-down menu 
+##NOTE: this is only the first page, if you want sequencing primers, must click individual links 
+
 #Date: Nov. 22, 2017
 #Author: Janice Love
 
