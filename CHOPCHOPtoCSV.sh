7d95aa30e3dc64ad3cd2bdcc6f654723c44e280a
Janice Love <janice.m.love@gmail.com>
2017-11-22 14:27:58 -0500

    Delete CHOPCHOPtoCSV.sh
    delete CHOPCHOPtoCSV.sh as is unnecessary

--- CHOPCHOPtoCSV.sh
+++
@@ -1,8 +0,0 @@
-#script to convert CHOP CHOP results table to proper csv functional file
-#Select "results table" from "download results" drop-down menu 
-##NOTE: this is only the first page, if you want sequencing primers, must click individual links 
-
-#Date: Nov. 22, 2017
-#Author: Janice Love
-
-cat $1 | sed 'y/" "/","/' > $2
